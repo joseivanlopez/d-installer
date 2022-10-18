1fe4f3a71ed14e5d6e556b2ac848cba9a720da37
Ancor Gonzalez Sosa <ancor@suse.de>
2022-10-18 17:16:52 +0200

    WIP: improved API for storage

--- service/lib/dinstaller/storage/proposal.rb
+++ service/lib/dinstaller/storage/proposal.rb
@@ -24,6 +24,7 @@ require "y2storage/guided_proposal"
 require "y2storage/proposal_settings"
 require "y2storage/dialogs/guided_setup/helpers/disk"
 require "dinstaller/with_progress"
+require "dinstaller/storage/volume"
 
 module DInstaller
   module Storage
@@ -98,22 +99,26 @@ module DInstaller
         proposal.settings.use_lvm
       end
 
+      def volumes
+        raise NoProposalError unless proposal
+
+        Volume.from_proposal(proposal)
+      end
+
       # Calculates a new proposal
       #
       # @param settings [Hash] settings to calculate the proposal
       #   (e.g., { "use_lvm" => true, "candidate_devices" => ["/dev/sda"]}). Note that keys should
       #   match with a public setter.
-      #
+      # @param volumes [Array<Volume>] only these properties will be honored
+      #   mount_point, filesystem_type, fixed_size_limits, min_size, max_size, snapshots
       # @return [Boolean] whether the proposal was correctly calculated
-      def calculate(settings = {})
-        proposal_settings = generate_proposal_settings(settings)
+      def calculate(settings = {}, volumes: nil)
+        proposal_settings = generate_proposal_settings(settings, volumes)
 
-        @proposal = Y2Storage::GuidedProposal.initial(
-          settings:      proposal_settings,
-          devicegraph:   probed_devicegraph,
-          disk_analyzer: disk_analyzer
-        )
-        save
+        @proposal = new_proposal(proposal_settings)
+        storage_manager.proposal = proposal
+
         changed!
 
         !proposal.failed?
@@ -140,16 +145,26 @@ module DInstaller
       # @return [Y2Storage::InitialGuidedProposal]
       attr_reader :proposal
 
+      def new_proposal(proposal_settings)
+        guided = Y2Storage::MinGuidedProposal.new(
+          settings:      proposal_settings,
+          devicegraph:   probed_devicegraph,
+          disk_analyzer: disk_analyzer
+        )
+        guided.propose
+        guided
+      end
+
       # Generates proposal settings from the given values
       #
       # @param settings [Hash]
+      # @param volumes [Array<Volume>, nil] ignored if nil or empty
       # @return [Y2Storage::ProposalSettings]
-      def generate_proposal_settings(settings)
+      def generate_proposal_settings(settings, volumes)
         proposal_settings = Y2Storage::ProposalSettings.new_for_current_product
 
-        config_volumes = read_config_volumes
-        # If no volumes are specified, just leave the default ones (hardcoded at Y2Storage)
-        proposal_settings.volumes = config_volumes unless config_volumes.empty?
+        init_config_volumes(proposal_settings)
+        Volume.adapt_settings(proposal_settings, volumes) if volumes&.any?
 
         settings.each { |k, v| proposal_settings.public_send("#{k}=", v) }
 
@@ -164,13 +179,12 @@ module DInstaller
         vols.map { |v| Y2Storage::VolumeSpecification.new(v) }
       end
 
-      # Saves the proposal or restores initial devices if a proposal was not calculated
-      def save
-        if proposal.failed?
-          storage_manager.staging = probed_devicegraph.dup
-        else
-          storage_manager.proposal = proposal
-        end
+      def init_config_volumes(proposal_settings)
+        vol_specs = read_config_volumes
+        # If no volumes are specified, just leave the default ones (hardcoded at Y2Storage)
+        return if vol_specs.empty?
+
+        proposal_settings.volumes = vol_specs
       end
 
       # @return [Y2Storage::DiskAnalyzer]
---
+++ service/lib/dinstaller/storage/volume.rb
@@ -0,0 +1,213 @@
+# frozen_string_literal: true
+
+# Copyright (c) [2022] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "pathname"
+
+module DInstaller
+  module Storage
+    # Backend class to represent a volume blah
+    class Volume
+      class << self
+        # When the proposal is already calculated, to show them
+        def from_proposal(proposal)
+          proposal.settings.volumes.select(&:proposed).map do |spec|
+            vol = new(proposal)
+            vol.specification = spec
+            vol
+          end
+        end
+
+        # When generating the proposal settings
+        def adapt_settings(proposal_settings, volumes)
+          proposal_settings.volumes.each do |spec|
+            next unless spec.proposed_configurable?
+            next if volumes.any? {|v| same_path?(v.mount_point, spec.mount_point) }
+            spec.proposed = false
+          end
+
+          volumes.each do |vol|
+            spec = proposal_settings.volumes.find {|v| same_path?(v.mount_point, vol.mount_point) }
+            if spec
+              proposal_settings.volumes.delete(spec)
+              spec.proposed = true
+              vol.specification = spec
+            end
+            proposal_settings.volumes.unshift(vol.specification)
+          end
+        end
+
+        def same_path?(path1, path2)
+          return false if path1.nil? || path2.nil?
+
+          Pathname.new(path1).cleanpath == Pathname.new(path2).cleanpath 
+        end
+      end
+
+      def initialize(proposal = nil)
+        @proposal = proposal
+      end
+
+      attr_writer :mount_point
+      attr_writer :fixed_size_limits
+      attr_writer :min_size
+      attr_writer :max_size
+      attr_writer :fs_type
+      attr_writer :snapshots
+      attr_reader :proposal
+
+      # This syncs the specification, so maybe a setter is not the best way
+      def specification=(spec)
+        @specification = spec
+        sync_to_specification
+      end
+
+      def specification
+        return @specification if @specification
+
+        # Maybe raise if no mount_point
+        raise "No mount point" if @mount_point.nil?
+
+        self.specification = Y2Storage::VolumeSpecification.new(mount_point: @mount_point)
+        @specification
+      end
+
+      def sync_to_specification
+        # Maybe check whether fixed makes sense
+        if @fixed_size_limits
+          @specification.ignore_fallback_sizes = true
+          @specification.ignore_snapshots_sizes = true
+          @specification.min_size = @min_size if @min_size
+          @specification.max_size = @max_size if @max_size
+        elsif @fixed_size_limits == false
+          @specification.ignore_fallback_sizes = false
+          @specification.ignore_snapshots_sizes = false
+        end
+        # Maybe some validation?
+        @specification.fs_type = @filesystem_type if @filesystem_type
+        @specification.snapshots = @snapshots unless @snapshots.nil?
+      end
+
+      def mount_point
+        specification&.mount_point || @mount_point
+      end
+
+      # FIXME: should this always be true if adaptative_sizes? if false
+      def fixed_size_limits
+        return @fixed_size_limits unless specification
+        return true unless adaptative_sizes?
+
+        # We can check either #ignore_fallback_size or #ignore_snapshots_size, both are in sync
+        specification.ignore_fallback_sizes
+      end
+
+      alias_method :fixed_size_limits?, :fixed_size_limits
+
+      # @return [Y2Storage::DiskSize]
+      def min_size
+        planned_device&.min || specification&.min_size || @min_size
+      end
+
+      # @return [Y2Storage::DiskSize]
+      def max_size
+        planned_device&.max || specification&.max_size || @max_size
+      end
+
+      # First simplistic approach using symbols and respond_to?
+      # @return [Symbol]
+      def device_type
+        return nil unless planned_device
+
+        planned_device.respond_to?(:lv_type) ? :logical_volume : :partition
+      end
+
+      def filesystem_type
+        specification&.fs_type || @filesystem_type
+      end
+
+      def filesystem_types
+        specification&.fs_types || []
+      end
+
+      def snapshots
+        specification&.snapshots || @snapshots
+      end
+
+      alias_method :snapshots?, :snapshots
+
+      def snapshots_configurable?
+        specification.snapshots_configurable
+      end
+
+      # Whether the device is encrypted directly or indirectly (eg. a LV in an encrypted LVM VG)
+      def encrypted?
+        # This implementation is not future-proof because does not allow to mix encrypted and not
+        # encrypted devices, but it will have to serve for now
+        proposal.settings.use_encryption
+      end
+
+      # Whether it makes sense to have automatic size limits for this particular volume
+      def adaptative_sizes?
+        # FIXME this should be a responsibility of the Proposal (since it's calculated by
+        # Proposal::DevicesPlanner)
+        snapshots_affect_sizes? || size_relevant_volumes.any?
+      end
+
+      # Whether snapshots affect the automatic calculation of the size limits
+      def snapshots_affect_sizes?
+        # FIXME this should be a responsibility of the Proposal (since it's calculated by
+        # Proposal::DevicesPlanner)
+        return false unless specification
+        return false unless specification.snapshots || specification.snapshots_configurable
+        return true if specification.snapshots_size && !specification.snapshots_size.zero?
+
+        specification.snapshots_percentage && !specification.snapshots_percentage.zero?
+      end
+
+      # Related volumes that may affect the calculation of the automatic size limits
+      # @return [Array<String>]
+      def size_relevant_volumes
+        # FIXME this should be a responsibility of the Proposal (since it's calculated by
+        # Proposal::DevicesPlanner)
+        @size_relevant_volumes ||=
+          # FIXME: not sure if the deleted volumes will remain there (as proposed = false)
+          proposal.settings.volumes.select { |v| fallback?(v, mount_point) }.map(&:mount_point)
+      end
+
+    private
+
+      def planned_device
+        @planned_device ||= proposal.planned_devices.find do |dev|
+          dev.respond_to?(:mount_point) && same_path?(dev.mount_point, mount_point)
+        end
+      end
+
+      def same_path?(*args)
+        self.class.same_path?(*args)
+      end
+
+      def fallback?(spec, mount_point)
+        same_path?(spec.fallback_for_min_size, mount_point) ||
+          same_path?(spec.fallback_for_max_size, mount_point) ||
+          same_path?(spec.fallback_for_max_size_lvm, mount_point)
+      end
+    end
+  end
+end
