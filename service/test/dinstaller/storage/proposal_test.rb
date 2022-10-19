2d0125d3adefe93842fc89909e3088d6907ec559
Ancor Gonzalez Sosa <ancor@suse.de>
2022-10-19 14:24:27 +0200

    WIP: temporary test to play with DInstaller::Storage::Proposal

--- service/test/dinstaller/storage/proposal_test.rb
+++ service/test/dinstaller/storage/proposal_test.rb
@@ -28,31 +28,30 @@ describe DInstaller::Storage::Proposal do
 
   let(:logger) { Logger.new($stdout, level: :warn) }
   let(:config) { DInstaller::Config.new(config_data) }
-  let(:y2storage_proposal) { instance_double(Y2Storage::GuidedProposal, failed?: failed) }
-  let(:y2storage_manager) do
-    instance_double(Y2Storage::StorageManager, probed: nil, probed_disk_analyzer: nil)
-  end
   let(:failed) { false }
   let(:config_data) { {} }
 
-  describe "#calculate" do
-    before do
-      allow(Y2Storage::StorageManager).to receive(:instance).and_return y2storage_manager
-      allow(Y2Storage::GuidedProposal).to receive(:initial).and_return y2storage_proposal
-      allow(y2storage_manager).to receive(:proposal=)
-    end
+  before do
+    path = File.join(FIXTURES_PATH, "empty-hd-50GiB.yaml")
+    Y2Storage::StorageManager.create_test_instance.probe_from_yaml(path)
 
+    allow_any_instance_of(Y2Storage::BlkDevice).to receive(:hwinfo).and_return(Y2Storage::HWInfoDisk.new)
+  end
+
+  describe "#calculate" do
     context "when there is no 'volumes' section in the config" do
       let(:config_data) { {} }
 
       it "calculates the Y2Storage proposal with a default set of VolumeSpecification" do
-        expect(Y2Storage::GuidedProposal).to receive(:initial) do |**args|
+        original_new = Y2Storage::MinGuidedProposal.method(:new)
+
+        expect(Y2Storage::MinGuidedProposal).to receive(:new) do |**args|
           expect(args[:settings]).to be_a(Y2Storage::ProposalSettings)
           vols = args[:settings].volumes
           expect(vols).to_not be_empty
           expect(vols).to all(be_a(Y2Storage::VolumeSpecification))
 
-          y2storage_proposal
+          original_new.call(**args)
         end
 
         proposal.calculate
@@ -61,24 +60,51 @@ describe DInstaller::Storage::Proposal do
 
     context "when there is a 'volumes' section in the config" do
       let(:config_data) do
-        { "storage" => { "volumes" => [{ "mount_point" => "/one" }, { "mount_point" => "/two" }] } }
+        { 
+          "storage" => {
+            "volumes" => [
+              {
+                "mount_point" => "/", "fs_type" => "btrfs", "min_size" => "10 GiB",
+                "snapshots" => true, "snapshots_percentage" => "300"
+              },
+              {
+                "mount_point" => "/two", "fs_type" => "xfs", "min_size" => "5 GiB",
+                "proposed_configurable" => true
+              }
+            ]
+          }
+        }
       end
 
       it "calculates the Y2Storage with the correct set of VolumeSpecification" do
-        expect(Y2Storage::GuidedProposal).to receive(:initial) do |**args|
+        original_new = Y2Storage::MinGuidedProposal.method(:new)
+
+        expect(Y2Storage::MinGuidedProposal).to receive(:new) do |**args|
           expect(args[:settings]).to be_a(Y2Storage::ProposalSettings)
           vols = args[:settings].volumes
           expect(vols).to all(be_a(Y2Storage::VolumeSpecification))
-          expect(vols.map(&:mount_point)).to contain_exactly("/one", "/two")
+          expect(vols.map(&:mount_point)).to contain_exactly("/", "/two")
 
-          y2storage_proposal
+          original_new.call(**args)
         end
 
         proposal.calculate
       end
+
+      it "manual WIP case for testing that overrides works" do
+        root = DInstaller::Storage::Volume.new
+        root.mount_point = "/"
+        root.snapshots = true
+
+        proposal.calculate({"use_lvm" => true}, volumes: [root])
+        expect(proposal.volumes.size).to eq 1
+        expect(proposal.volumes.first.fixed_size_limits).to eq false
+        #byebug
+        puts "The usual puts"
+      end
     end
 
-    context "when the Y2Storage proposal successes" do
+    xcontext "when the Y2Storage proposal successes" do
       let(:failed) { false }
 
       it "saves the proposal" do
@@ -87,7 +113,7 @@ describe DInstaller::Storage::Proposal do
       end
     end
 
-    context "when the Y2Storage proposal fails" do
+    xcontext "when the Y2Storage proposal fails" do
       let(:failed) { true }
 
       it "does not save the proposal" do
